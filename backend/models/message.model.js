1656b63d3fa8160675ec15dcd9eacd8b807e9ba5
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-09-13 18:35:28 +0530

    first commit

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -1,23 +1,26 @@
 import mongoose from "mongoose";
 
-const messageSchema = new mongoose.Schema({
-    senderId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    receiverId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required : true
-    },
-    message:{
-        type: String,
-        required: true
-    }
-    // createdAt , updateAt 
-}, {timestamps: true});
+const messageSchema = new mongoose.Schema(
+	{
+		senderId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		receiverId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		message: {
+			type: String,
+			required: true,
+		},
+		// createdAt, updatedAt
+	},
+	{ timestamps: true }
+);
 
-const Message = mongoose.model("Message",messageSchema);
+const Message = mongoose.model("Message", messageSchema);
 
-export default Message; 
+export default Message;
