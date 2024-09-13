d0da004ffe18e671397d5c59f95099f330139869
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-09-13 18:33:41 +0530

    Add tailwindcss

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // eslint-disable-next-line no-undef
+    require('daisyui'),
+  ],
+}
