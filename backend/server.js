d47dc45361573015a531228b0cceea522eb7d4d9
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-09-13 18:27:42 +0530

    Add user auth

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";            
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json());  //to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
+
+app.use("/api/auth",authRoutes);   
+app.use("/api/messages",messageRoutes); 
+
+// app.get("/",(req, res) =>{
+//     // root route https://localhost:5000/
+//     res.send("Hello World!!");
+// })
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server running on port ${PORT}`);
+    
+});
