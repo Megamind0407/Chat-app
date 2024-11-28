ac96468635fe80c61e4812d169768971e050397c
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-11-28 16:03:50 +0530

    updated node packages

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
 
-
 const connectToMongoDB = async () => {
-    
     try {
         await mongoose.connect(process.env.MONGO_URL);
         console.log("Connected to MongoDB");
     } catch (error) {
-        console.log("Error connecting to MongoDB",error.message);
+        console.log("Error connecting to MongoDB:", error.message);
     }
 };
 
