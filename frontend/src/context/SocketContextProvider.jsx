4ae1cb883b8d805607f38a277f896cb74bf19271
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-12-01 00:44:17 +0530

    update url

--- frontend/src/context/SocketContextProvider.jsx
+++ frontend/src/context/SocketContextProvider.jsx
@@ -12,8 +12,8 @@ export const SocketContextProvider = ({ children }) => {
         if (authUser) {
             // eslint-disable-next-line no-undef
             const SERVER_URL = process.env.NODE_ENV === "production"
-                ? "https://chat-app-ict4.onrender.com"
-                : "http://localhost:5000";
+                "https://chat-app-ict4.onrender.com"
+                ?? "http://localhost:5000";
 
             return io(SERVER_URL, {
                 transports: ["websocket"],
